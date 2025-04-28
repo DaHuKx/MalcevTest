f2c9bf802f9800d2aef9eb1c8143de146ec4cd73
Даниил <dmalcev@rit-it.com>
2025-04-28 15:58:03 +0700

    Added handling for bad log files

--- LogFormatter/Program.cs
+++ LogFormatter/Program.cs
@@ -6,9 +6,13 @@ var logs = await resolver.ParseLogsAsync("E:\\Проекты\\MalcevTest\\LogFor
 
 if (logs is null || logs.Any())
 {
+    Console.WriteLine("File with logs is not valid.");
+    return;
 }
 
 foreach (var log in logs)
 {
     await LogWriter.WriteLogAsync(log);
 }
+
+Console.WriteLine("Complete");
